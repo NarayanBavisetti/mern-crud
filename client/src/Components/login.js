760d68229b750b8fc2a821115ad16376a6b3a52b
narayan <narayan3119@gmail.com>
2021-06-10 20:47:01 +0530

    added a server

--- client/src/Components/login.js
+++ client/src/Components/login.js
@@ -1,19 +1,80 @@
-import React from 'react'
-import {Button, FormGroup, Input, Typography } from '@material-ui/core'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Button,
+  FormGroup,
+  Grid,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
-export default function login() {
-    return (
-        <div>
-            welcome to login page
-            <FormGroup>
-            <Input placeholder="email" required>email</Input>
-            <Input placeholder="password" required>Password</Input>
-            <Typography >Do not have an account ?
-            <Link to="/signup">Sign Up</Link>
-             </Typography>
-            <Button variant="secondary">Log In</Button>
-            </FormGroup>
-        </div>
-    )
-}
+const useStyles = makeStyles({
+  paper: {
+    height: "60vh",
+    width: 350,
+    padding: 20,
+    margin: "40px auto",
+  },
+  logo:{
+      background: "#5959f7"
+  },
+  gap:{
+      margin:"8px 0px"
+  },
+  gap1:{
+      margin:"4px 20px",
+     
+  },
+  gap3:{
+      margin:"15px 0px"
+  },
+  gap4:{
+      margin:"12px 0px"
+  }
+});
+const Login = () => {
+  const [count ,setCount] = useState('')
+  const classes = useStyles();
+
+useEffect(() => {
+  updateVisitorCount()
+},[])
+
+  function updateVisitorCount() {
+    fetch("https://api.countapi.xyz/update/narayan/mern-crud/?amount=1")
+    .then(res => res.json())
+    .then(res => {
+      setCount(res.value)
+    })
+   
+  }
+  return (
+    <div>
+      <Grid>
+        <Paper elevation={10} className={classes.paper}>
+            <Grid align="center" className={classes.gap3}>
+          <Avatar className={classes.logo}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Log In</h2>
+          </Grid>
+          <FormGroup >
+          <TextField className={classes.gap} id="outlined-basic" label="email" placeholder="enter email" variant="outlined" fullWidth required />
+          <TextField className={classes.gap} id="outlined-basic" label="password" placeholder="enter password" type="password" variant="outlined" required />
+
+            <Typography className={classes.gap1}>
+              Do not have an account ? <Link to="/signup"> Sign Up</Link>
+            </Typography>
+            <Button className={classes.gap4} type="submit" variant="contained" color="secondary" fullWidth>Log In</Button>
+          </FormGroup>
+          <Typography>No of people visited this page {count}</Typography>
+        </Paper>
+      </Grid>
+    </div>
+  );
+};
+export default Login;
